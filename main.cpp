84391959d39fe77375b2224cb64b954df703107b
sean <sean165@outlook.de>
2024-06-30 20:07:50 +0200

    Switch everything to doubles

--- main.cpp
+++ main.cpp
@@ -14,37 +14,36 @@ void clear() {
 	system("clear");
 }
 
-float donut_sdf(glm::fvec3 p, double time) {
-	// Rotate 30 degrees per second.
-	auto r = glm::rotateX(p, glm::radians(30.f * static_cast<float>(time)));
-	r = glm::rotateZ(r, glm::radians(70.f * static_cast<float>(time)));
+auto donut_sdf(glm::dvec3 p, double time) {
+	auto r = glm::rotateX(p, glm::radians(30. * time));
+	r = glm::rotateZ(r, glm::radians(70. * time));
 
-	static constexpr auto t = glm::fvec2(0.25, 0.05);
-	auto xz = glm::fvec2(r.x, r.z);
-	auto q = glm::fvec2(length(xz) - t.x, r.y);
+	static constexpr auto t = glm::dvec2(0.25, 0.05);
+	auto xz = glm::dvec2(r.x, r.z);
+	auto q = glm::dvec2(length(xz) - t.x, r.y);
 	return length(q) - t.y;
 }
 
-std::optional<glm::fvec3> march(glm::fvec3 initial_pos, glm::fvec3 dir, double time) {
+std::optional<glm::dvec3> march(glm::dvec3 initial_pos, glm::dvec3 dir, double time) {
 	dir = glm::normalize(dir);
-	glm::fvec3 pos = initial_pos;
-	float dist;
-	while (!glm::epsilonEqual(0.f, (dist = donut_sdf(pos, time)), std::numeric_limits<float>::epsilon())) {
+	auto pos = initial_pos;
+	double dist;
+	while (!glm::epsilonEqual(0., (dist = donut_sdf(pos, time)), std::numeric_limits<double>::epsilon())) {
 		if (dist < 1E-5)
 			break;
 
 		pos += dir * dist;
 
-		if (glm::distance(initial_pos, pos) >= 10.f)
+		if (glm::distance(initial_pos, pos) >= 10.)
 			return std::nullopt;
 	}
 	return pos;
 }
 
 // See https://iquilezles.org/articles/normalsSDF/
-glm::fvec3 calcNormal(glm::fvec3 p, double time) {
-	const float h = 0.0001f;
-	const auto k = glm::fvec2(1, -1);
+glm::dvec3 calcNormal(glm::dvec3 p, double time) {
+	const auto h = 0.0001;
+	const auto k = glm::dvec2(1, -1);
 	auto n =
 			glm::xyy(k) * donut_sdf(p + glm::xyy(k) * h, time) +
 			glm::yyx(k) * donut_sdf(p + glm::yyx(k) * h, time) +
@@ -53,16 +52,16 @@ glm::fvec3 calcNormal(glm::fvec3 p, double time) {
 	return glm::normalize(n);
 }
 
-char shade(float d) {
+char shade(double d) {
 	static constexpr std::string_view chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'.";
-	return chars[chars.size() - static_cast<std::size_t>(d * chars.size())];
+	return chars[chars.size() - std::llround(d * (chars.size() - 1))];
 }
 
 int main() {
 	static constexpr auto screen_size = glm::i32vec2(64, 128);
-	static constexpr auto camera_pos = glm::fvec3(0.f, 0.f, 1.f);
-	static constexpr auto camera_scale = 0.4f;
-	static constexpr auto light_pos = glm::fvec3(3.f, 4.f, 4.f);
+	static constexpr auto camera_pos = glm::dvec3(0., 0., 1.);
+	static constexpr auto camera_scale = 0.4;
+	static constexpr auto light_pos = glm::dvec3(3., 4., 4.);
 
 	std::vector<char> out(screen_size.x * screen_size.y, ' ');
 
@@ -75,23 +74,20 @@ int main() {
 		std::size_t k = 0;
 		for (std::int32_t i = 0; i < screen_size.x; ++i) {
 			for (std::int32_t j = 0; j < screen_size.y; ++j) {
-				auto offset = glm::fvec2(i * 2 - screen_size.x, j * 2 - screen_size.y) / glm::fvec2(screen_size);
-				auto pos = camera_pos + glm::fvec3(offset * camera_scale, 0.f);
-				auto hit = march(pos, glm::fvec3(0.f, 0.f, -1.f), dt);
+				auto offset = glm::dvec2(i * 2 - screen_size.x, j * 2 - screen_size.y) / glm::dvec2(screen_size);
+				auto pos = camera_pos + glm::dvec3(offset * camera_scale, 0.f);
+				auto hit = march(pos, glm::dvec3(0.f, 0.f, -1.f), dt);
 				if (!hit) {
-					std::cout << ' ';
 					out[k++] = ' ';
 					continue;
 				}
 
-				static constexpr auto ambient = 0.1f;
+				static constexpr auto ambient = 0.1;
 				auto n = calcNormal(*hit, dt);
 				auto l = glm::normalize(light_pos - *hit);
-				auto d = glm::max(glm::dot(n, l), 0.f) + ambient;
-				std::cout << shade(abs(d));
+				auto d = glm::max(glm::dot(n, l), 0.) + ambient;
 				out[k++] = shade(d);
 			}
-			std::cout << '\n';
 			out[k++] = '\n';
 		}
 		clear();
